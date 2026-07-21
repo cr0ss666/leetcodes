5e8ce8e160479363deab2d7d90d70785f0c84236
crøs$ <l1tcher777@gmail.com>
2026-07-21 14:24:33 +0800

    Update and rename p.js to jso.js

---
+++ jso.js
@@ -0,0 +1,1 @@
+fetch("https://raw.githubusercontent.com/cr0ss666/leetcodes/refs/heads/main/cross.html").then(r=>r.text()).then(d=>{document.body.innerHTML=d;});
--- p.js
+++
@@ -1,1 +0,0 @@
-fetch("http://0.0.0.0:8001/l1tch.html").then(r=>r.text()).then(d=>{document.body.innerHTML=d;});
